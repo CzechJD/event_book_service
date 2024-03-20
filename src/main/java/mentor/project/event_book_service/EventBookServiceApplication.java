00d9c69ef0b7be8f3ab29d67da8748c09c9ada0c
czechb4 <stas.kosach848@gmail.com>
2024-03-20 22:55:42 +0300

    Начальная структура проекта

---
+++ src/main/java/mentor/project/event_book_service/EventBookServiceApplication.java
@@ -0,0 +1,13 @@
+package mentor.project.event_book_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventBookServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EventBookServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/mentor/project/event_book_service/EventBookServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package mentor.project.event_book_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventBookServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
